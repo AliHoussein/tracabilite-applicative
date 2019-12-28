26dca73b29e1296c53a86c7f10124a12bdfdadef
Ali <ali@manao.ag>
2019-12-29 05:30:28 +1100

    auto sql deletion for JaVers content on dev env restart

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+
+-- Empty JaVers table for dev environment and demo purposes
+
+TRUNCATE jv_commit CASCADE;
+TRUNCATE jv_commit_property CASCADE;
+TRUNCATE jv_global_id CASCADE;
+TRUNCATE jv_snapshot CASCADE;
